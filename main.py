d84fc0cb943dbd51a688de344d05c017dfc88fbe
Filipe Breda <filipe.breda@ctw.bmwgroup.com>
2024-02-19 13:41:08 +0000

    Added implementation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from clients.forum_client import ForumClient
 
 def main():
     forum_client = ForumClient("jsonplaceholder.typicode.com/")
-    print(forum_client.get_posts())
+    print(forum_client.get_user_posts(1))
 
 
 if __name__ == '__main__':
